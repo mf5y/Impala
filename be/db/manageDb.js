b76f0e6e2fbc152015d0864d0f1414bbbee81543
Pseudophpt <pseudophpt@gmail.com>
2018-02-05 01:28:33 -0500

    Commit V2: I forgot I had a git repo and this thing is broken

--- be/db/manageDb.js
+++ be/db/manageDb.js
@@ -1,44 +1,225 @@
-var redis = require('redis');
-var client = redis.createClient();
+var mongoClient = require('mongodb');
 var defaults = require('./defaults');
+var util = require('../util');
+var base32 = require('base32');
+var randomBytes = require('random-bytes');
 var sanitizeHtml = require('sanitize-html');
 
-/* Adds new list to database */
-module.exports.addList = function (name, description, owner) {
-  var nameSanitized = sanitizeHtml(name);
-  var nameFormatted = "lists:" + nameSanitized.replace('.', ':');
-  var parent = nameFormatted.split(":").slice(0, -1).join(":") + ":*";
+const url = 'mongodb://localhost:27017';
 
-  /* Test if it exists */
-  client.keys(parent, function (err, reply) {
-    if (reply.length > 0) {
-      client.hmset(nameFormatted,
-        'description', description,
-        'owner', owner,
-        'type', defaults.type);
-    }
-  })
+function checkDocumentExists (callback) {
+
+}
+
+/* Adds list to database */
+module.exports.addList = function (properties) {
+  /* Sanitize */
+  var propertiesSanitized = util.sanitize(properties);
+
+  var description = propertiesSanitized.owner;
+  var owner = propertiesSanitized.owner;
+
+  /* Remove special characters */
+  var name = propertiesSanitized.name.replace(/[^\w\s\.]/gi, '');
+  /* Remove last dot */
+  var parent = name.split('.').slice(0, -1).join('.');
+
+  return mongoClient.connect(url)
+    .then(client => {
+      /* Lists collection */
+      var db = client.db('local');
+      var lists = db.collection('lists');
+
+      /* Find parent */
+      return lists.find({
+        'name' : parent
+      }).toArray()
+        .then(parent => {
+          /* If parent exists */
+          if (parent.length > 0) {
+            var list = { 'name': name,
+              'description': description,
+              'owner': owner };
+            /* Add list */
+            return lists.update(list,
+              list,
+              { 'upsert' : true });
+          }
+          /* Otherwise throw error */
+          else return Promise.reject(new Error ('Parent list not found!'))
+        });
+    });
 }
 
 /* Removes list from database */
 module.exports.delList = function (name) {
+  /* Sanitize */
   var nameSanitized = sanitizeHtml(name);
-  var nameFormatted = "lists:" + nameSanitized.replace('.', ':');
 
-  client.del(nameFormatted);
+  return mongoClient.connect(url)
+    .then(client => {
+      /* Lists collection */
+      var db = client.db('local');
+      var lists = db.collection('lists');
+
+      return lists.deleteOne({ 'name' : nameSanitized });
+    })
 }
 
-/* Gets child lists */
-module.exports.getChildLists = function (name, callback) {
-  var nameSanitized = sanitizeHtml(name);
-  var nameFormatted = "lists:" + nameSanitized.replace('.', ':') + ':*';
+/* Adds thread to database */
+module.exports.addThread = function (list, properties) {
+  /* Random code */
+  var code = base32.encode(randomBytes.sync(8));
 
-  client.keys(nameFormatted, callback);
+  /* Sanitize */
+  var propertiesSanitized = util.sanitize(properties);
+
+  var listSanitized = sanitizeHtml(list);
+
+  /* Variables */
+  var date = new Date();
+
+  var name = propertiesSanitized.name;
+  var text = propertiesSanitized.text;
+  var email = propertiesSanitized.email;
+  var subject = propertiesSanitized.subject;
+
+  return mongoClient.connect(url)
+    .then(client => {
+      /* Posts collection */
+      var db = client.db('local');
+      var posts = db.collection('posts');
+      var threads = db.collection('threads');
+
+      /* Insert thread */
+      return threads
+        .insert({
+          'list': listSanitized,
+          'id': code,
+          'author': name,
+          'subject': subject,
+          'bump': date,
+          'bumper': name
+        }).then(() => {
+          /* Insert post */
+          return posts.insert({
+            'list': listSanitized,
+            'id': code,
+            'threadID': code,
+            'name': name,
+            'text': text,
+            'email': email,
+            'subject': subject,
+            'date': date
+          });
+        });
+    });
+}
+/* Adds post to database */
+module.exports.addPost = function (list, thread, properties) {
+  /* Random code */
+  var code = base32.encode(randomBytes.sync(8));
+
+  /* Sanitize */
+  var propertiesSanitized = util.sanitize(properties);
+
+  var listSanitized = sanitizeHtml(list);
+  var threadSanitized = sanitizeHtml(thread);
+
+  /* Variables */
+  var date = new Date();
+
+  var name = propertiesSanitized.name;
+  var text = propertiesSanitized.text;
+  var email = propertiesSanitized.email;
+  var subject = propertiesSanitized.subject;
+
+  return mongoClient.connect(url)
+    .then(client => {
+      /* Posts collection */
+      var db = client.db('local');
+      var posts = db.collection('posts');
+      var threads = db.collection('threads');
+
+      /* Make sure thread exists */
+      return threads.find({
+        'id': threadSanitized,
+        'list': listSanitized
+      }).toArray()
+        .then(threads => {
+          /* If parent exists */
+          if (threads.length > 0) {
+            /* Add list */
+            return posts.insert({
+              'list': listSanitized,
+              'id': code,
+              'threadID': threadSanitized,
+              'name': name,
+              'text': text,
+              'email': email,
+              'subject': subject,
+              'date': date
+            });
+          }
+          /* Otherwise throw error */
+          else return Promise.reject(new Error ('Thread not found!'))
+        }).then(() => {
+          /* Update thread */
+          return threads.update({
+            'id': threadSanitized,
+            'list': listSanitized
+          }, {
+            $set: {
+              'bump': date,
+              'bumper': name
+            }
+          })
+        });
+    });
 }
 
-module.exports.getProperties = function (name, callback) {
-  var nameSanitized = sanitizeHtml(name);
-  var nameFormatted = "lists:" + nameSanitized.replace('.', ':');
+/* Get threads in list */
+module.exports.getThreads = function(list) {
+  /* Sanitize */
+  var listSanitized = sanitizeHtml(list);
 
-  client.hgetall(nameFormatted, callback);
+  return mongoClient.connect(url)
+    .then(client => {
+      /* Posts collection */
+      var db = client.db('local');
+      var threads = db.collection('threads');
+
+      /* Get threads */
+      return threads.find({
+        'list': listSanitized
+      }).toArray();
+    });
+}
+
+/* Get posts in thread */
+module.exports.getPosts = function(list, thread) {
+  /* Sanitize */
+  var listSanitized = sanitizeHtml(list);
+  var threadSanitized = sanitizeHtml(thread);
+
+  return mongoClient.connect(url)
+    .then(client => {
+      /* Posts collection */
+      var db = client.db('local');
+      var posts = db.collection('posts');
+
+      /* Get posts */
+      return posts.find({
+        'list': listSanitized,
+        'threadID': threadSanitized
+      }).sort({ date: 1 })
+        .toArray().then(threadPosts => {
+        /* If there are posts */
+        if (threadPosts.length > 0) {
+          return Promise.resolve(threadPosts);
+        }
+        /* Otherwise error */
+        else return Promise.reject(new Error ('Thread not found!'));
+      });
+    });
 }
---
+++ fe/public/javascripts/userScripts.js
@@ -0,0 +1,31 @@
+/* Called on reply button click */
+function copyReply (id) {
+  /* Get the body of the post */
+  var post = document.getElementById(id).children[1];
+  var body = post.children[1];
+  var postSubject = post.children[0].children[3].children[1].innerHTML;
+  var postName = post.children[0].children[1].children[1].innerHTML;
+  var lines = body.innerHTML.split('</p><p>');
+
+  /* Remove extra tags */
+  lines[0] = lines[0].substring(3);
+  lines[lines.length - 1] = lines[lines.length - 1].substring(
+    0,
+    lines[lines.length - 1].length - 4
+  );
+
+  var textarea = document.getElementById('text');
+  var subject = document.getElementById('userSubject');
+
+  /* Add reply name */
+  lines.unshift('');
+  lines.unshift(postName + ' said:');
+
+  /* Add quoted material to text box */
+  for (var i = 0; i < lines.length; i ++) {
+    textarea.value += ('> ' + lines[i] + '\r\n');
+  }
+
+  /* Set subject */
+  subject.value = 'Re: ' + postSubject;
+}
